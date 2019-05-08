0ea3bf7049d2df0446af9c0ed31af3aa1ac2b7ee
MirandaH99 <mirandahanks00@gmail.com>
2019-05-08 14:54:15 -0400

    All Current Enemy scripts
    They move and detect player, shoot does not work with shooter type enemy yet, working on it. Enemies do not take or deal damage yet.

--- Enemy Scripts/EnemyController.cs
+++ Enemy Scripts/EnemyController.cs
@@ -4,5 +4,308 @@ using UnityEngine;
 
 public class EnemyController : EnemyActor
 {
+   
+    public delegate void ThinkFunction();
+    public ThinkFunction think;
+
+    public bool LOSCheck;
+    public string STATEName;
+    public float DistanceSeenGO; 
+    public bool HasEnteredState = false;
+
+    //If AI shoots at player after seeing them, follows player but maintains certain distance
+    public float visonAngle = 90f;
+    public float visonRange = 5f;
+    public float outOfVisionRange = 10f;
+    public bool isShooter = false;
+    public float shootRange = 5;
+    float distanceToSeenGO;
+
+    float timer = 0f;
+    public float shootRate = 2f;
+    public bool useActorsOnly = true;
+
+    public GameObject SeenPlayerGO; // Game Object
+    public Pawn SeenPlayerPawn; // Pawn Object
+    public GameObject shootPos;
+    public GameObject shootPreFab;
+
+    public float speed;
+    public float SDistance;
+
+    public float waitTime;
+    public float startWaitTime;
+
+    public Transform[] PathPoints;
+    private int PathPointIndex = 0;
+
+    private GameObject target;
+
+    VisionControl vc;
+    public List<GameObject> SeenList;  
     
+
+    protected void Start()
+    {
+        vc = gameObject.GetComponent<VisionControl>();
+        vc.visAngle = visonAngle;
+        vc.visRange = visonRange;
+
+        waitTime = startWaitTime;
+
+        SetState(ThinkPatrol); 
+    }
+
+
+    protected void FixedUpdate()
+    {
+        if (think != null)
+        {
+            think.Invoke(); 
+        }
+
+    }
+
+
+    public void SetState (ThinkFunction newthink)
+    {
+        HasEnteredState = true;
+        think = newthink; 
+    }
+
+    public void ThinkPatrol()
+    {
+        if (HasEnteredState)
+        {
+            STATEName = "Patrol";
+            HasEnteredState = false;
+        }
+
+        UpdatePatrol();
+
+        // vison control sees a playerpawn object
+        SeenList = vc.PerformVision();
+        foreach (GameObject g in SeenList)
+        {
+            PlayerPawn pawn = g.GetComponentInParent<PlayerPawn>();
+
+            if (pawn)
+            {
+                SeenPlayerPawn = pawn; 
+                SeenPlayerGO = g; 
+                SetState(ThinkSee);
+                return; 
+            }
+        }
+    }
+
+    //Manages the patrol 
+    public void UpdatePatrol()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, PathPoints[PathPointIndex].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, PathPoints[PathPointIndex].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+               // Debug.Log("moving...");
+                PathPointIndex++;
+                waitTime = startWaitTime;
+                if (PathPointIndex >= PathPoints.Length)
+                {
+                    PathPointIndex = 0;
+                }
+            }
+            else
+            {
+               // Debug.Log("waiting...");
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void ThinkSee()
+    {
+        //Debug.Log("ThinkSee State");
+        if (HasEnteredState)
+        {
+            STATEName = "See";
+            HasEnteredState = false;
+        }
+        // If We have no Seen Player, return to Patrol 
+        if (SeenPlayerGO == null)
+        {
+            SetState(ThinkPatrol);
+        }
+
+        MoveToPlayer();
+
+        //Check LOS
+        // if Lost LOS, return to Patrol
+        // SeenPlayer = null; 
+
+        distanceToSeenGO = Vector2.Distance(transform.position, SeenPlayerGO.transform.position);
+
+        DistanceSeenGO = distanceToSeenGO;
+
+        // check vision Range
+        // if out of range,  return to Patrol
+
+        if (distanceToSeenGO > outOfVisionRange)
+        {
+            Debug.Log("I dont see player anymore");
+            SeenPlayerGO = null;
+            SeenPlayerPawn = null;
+            SetState(ThinkPatrol);
+            return;
+        }
+
+        Debug.Log("I still see player");
+
+        // if is a shooter 
+        // if in shoot range
+        // go to ThinkShoot; 
+
+        if (isShooter)
+        {
+            Debug.Log("I am a shooter!");
+            if (distanceToSeenGO < shootRange)
+            {
+                Debug.Log("Getting ready to shoot!");
+                SetState(ThinkShoot);
+                return;
+            }
+        }
+    }
+
+    //****TO DO  TO DO  TO DO****
+    // Manages anything related to moving to the player... 
+    public void MoveToPlayer()
+    {
+        Debug.Log("Moving toward player");
+        transform.position = Vector2.MoveTowards(transform.position, SeenPlayerGO.transform.position, speed * Time.deltaTime);
+    }
+
+    public void ThinkShoot()
+    {
+        Debug.Log("ThinkShoot State");
+
+        if (HasEnteredState)
+        {
+            STATEName = "Shoot";
+            HasEnteredState = false;
+        }
+        // If We have no Seen Player, return to Patrol 
+        if (SeenPlayerGO == null)
+        {
+            SetState(ThinkPatrol);
+        }
+
+        //Check LOS
+        // if Lost LOS, return to Patrol
+        // SeenPlayer = null; 
+        //if (!vc.HasLineOfSight(SeenPlayerGO))
+        //{
+        //    SeenPlayerGO = null;
+        //    SeenPlayerPawn = null;
+        //    SetState(ThinkPatrol);
+        //    return;
+        //}
+
+        if (distanceToSeenGO >= shootRange)
+        {
+            Debug.Log("I don't see anyone anymore...");
+            SetState(ThinkPatrol);
+            return;
+        }
+
+        //****TO DO  TO DO  TO DO****
+        // check shoot Range
+        // if in range, shoot
+        // Else return to ThinkSee
+        if (Vector2.Distance(transform.position, SeenPlayerGO.transform.position) <= shootRange)
+        {
+            Shoot();
+        }
+        else
+        {
+            SetState(ThinkSee);
+        }
+
+    }
+
+    
+    protected void Shoot()
+    {
+        GameObject newObj = Instantiate(shootPreFab, shootPos.transform.position, shootPos.transform.rotation);
+        
+        //****TO DO  TO DO  TO DO****
+        // Set Projectile move at player
+    }
+
+    /*
+    protected void FixedUpdate_old()
+
+    {
+        if (true)
+        {
+            if(movespot.Length==0)
+            {
+                Debug.Log("No movepoints, i'll just stay here...");
+                return;
+            }
+            transform.position = Vector2.MoveTowards(transform.position, movespot[random].position, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, movespot[random].position) < 0.2f)
+            {
+                if (waitTime <= 0)
+                {
+                    random = Random.Range(0, movespot.Length);
+                    waitTime = startWaitTime;
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+        }
+
+        SeenList = vc.PerformVision();
+        foreach (GameObject g in SeenList)
+        {
+            Actor a = g.GetComponentInParent<Actor>();
+
+            if (a)
+            {
+                isPatrolling = false;
+
+                Debug.Log("***" + gameObject.name + " found (m) " + g.name);
+                target = g.GetComponent<Transform>();
+
+                if(isFollowerAI)
+                {
+                    if (Vector2.Distance(transform.position, target.position) > SDistance)
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                    }
+                }
+                else if(isShooterAI)
+                {
+                    timer += Time.deltaTime;
+
+                    if (timer >= shootRate)
+                    {
+                        Shoot();
+                        timer = 0;
+                    }
+
+                    if (Vector2.Distance(transform.position, target.position) > SDistance)      //adjust so shooter follows but maintains certain distance
+                    {
+                        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+                    }
+                }
+            }
+        }
+        
+    }
+    */
+
 }
---
+++ Enemy Scripts/VisionControl.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisionControl : MonoBehaviour
+{
+    public float visGuyNum = 0;
+    //for test purposes
+
+    public float visRange = 5f;
+    //max distance allowed in range
+
+    public float CloseRange = 3f;
+    //Close Range
+
+    public float visAngle = 100f;
+    //Angle of View
+
+    float angle;
+
+    public bool usePlayersOnly = true;
+    //True: filter gameObjects that do not have Actor script or child of
+    //False: do not filter gameObjects
+    
+     RaycastHit hit;
+
+    public GameObject DEBUGTarget;
+    public GameObject DEBUGHitResult;
+    public bool DEBUGSameObjectBool; 
+
+    public List<GameObject> SeenList = new List<GameObject>();
+
+    Vector2 orig;
+
+    //Performs a ray cast from viewing obj
+    //True: LOS is blocked by another object
+    //False: LOS is clear
+    public bool HasLineOfSight(GameObject target)
+    {
+        DEBUGTarget = target;
+
+        Vector2 rayDir = target.transform.position - gameObject.transform.position; 
+
+        RaycastHit2D hit =
+            Physics2D.Raycast(gameObject.transform.position, rayDir, visRange);
+        if (hit)        //ray cast
+        {
+            DEBUGHitResult = hit.collider.gameObject;
+            DEBUGSameObjectBool = (hit.collider.gameObject == target);
+            return DEBUGSameObjectBool;
+            /*
+            //did I hit the target? 
+            if (hit.collider.gameObject == target)
+            {
+                //Debug.Log(hit.collider.name);
+                return true;
+            }
+        }
+    */
+        }
+        return false;
+ 
+       
+    }
+    
+    //performs vis check and returns a list of gameObjects in LOS o viewing obj
+    public List<GameObject> PerformVision()
+    {
+        orig = gameObject.transform.position;
+        List<GameObject> hitlist = new List<GameObject>();
+
+        // #1 Is this in Vis Range
+        Collider2D[] hits = Physics2D.OverlapCircleAll(orig, visRange);     //.OverlapSphere(orig, visRange); < 3D
+
+        //Debug.Log("Perform Vision");
+        // #2 Is this an Actor? 
+        foreach (Collider2D h in hits)
+        {
+            //Debug.Log("In Hitslist");           //Not getting here? IDK why?
+            GameObject g = h.gameObject; 
+            PlayerPawn a = g.GetComponentInParent<PlayerPawn>();
+            if(usePlayersOnly)
+            {
+                if (a)
+                {
+                    //Debug.Log(gameObject.name + " sees " + a.gameObject.name);
+                    //its an actor... hurray! '
+                    hitlist.Add(a.gameObject);
+                }
+                else
+                {
+                   // Debug.Log(g.name + " is not an actor");
+                    continue; 
+                }
+            }
+            else 
+            {             
+                hitlist.Add(g);
+            }
+
+            if(g.name==gameObject.name)
+            {
+                hitlist.Remove(g);
+            }
+
+            // #3 is in Close range
+            // if true break
+            Vector3 HeadingVector = g.transform.position - gameObject.transform.position; 
+            if (HeadingVector.magnitude <= CloseRange)
+            {
+                continue;
+            }
+
+            //or
+
+            // #4 IS in Vis Arc
+            // if false, remove from list & break 
+            angle = Vector3.Angle(Vector3.forward, Vector3.right);
+
+            // #5 Raycast LOS
+            // if false, remove from list & break           
+            if (!HasLineOfSight(g))
+            {
+                hitlist.Remove(g);
+            }           
+        }
+
+        return hitlist;   
+    }
+}
