3efb5318b0b6a0c3e2c0895debf09348074641ce
MirandaH99 <mirandahanks00@gmail.com>
2019-05-08 20:51:18 -0400

    updated things

---
+++ Enemy Scripts/EnemyPawn.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPawn : EnemyActor
+{
+    public const float maxHealth = 100f;
+    public float currentHealth = maxHealth;
+
+    Animator anim;
+    public RuntimeAnimatorController slimeAnims;
+
+    string direction = "South";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            checkDeath();
+            Debug.Log(gameObject.name + " is dead.");
+        }
+
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        currentHealth -= damageAmount;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+        }
+    }
+
+    public void checkDeath()
+    {
+        Destroy(gameObject);
+    }
+}
